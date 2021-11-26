9da4dd8ed538f43283f5ae83c4a1144789ebfbfa
Vishesh Kumar <visheshkumar41@gmail.com>
2021-11-26 16:46:17 +0530

    second commit

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -23,7 +23,7 @@ class FirstScreen extends StatelessWidget {
                 Navigator.pushNamed(context, '/second');
                 // Navigate back to the first screen by popping the current route
                 // off the stack.
-               // Navigator.pop(context);
+                // Navigator.pop(context);
               },
               child: const Text('Second Screen'),
             ),
